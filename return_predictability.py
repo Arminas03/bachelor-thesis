22f2159691047d648b68802d376b4fa748617776
Arminas03 <arminas.raz@gmail.com>
2025-05-23 17:26:59 +0200

    finish return predictability replication

--- return_predictability.py
+++ return_predictability.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import statsmodels.api as sm
 import numpy as np
+import json
 
 from constants import *
 from utils import get_data, transform_volatility_by_horizon
@@ -32,52 +33,85 @@ def get_jae_data():
 
 
 def trim_series(series: pd.Series, allowed_dates):
-    return series.loc[series.index.isin(allowed_dates)]
+    return series.loc[series.index.isin(allowed_dates)].to_numpy()
 
 
-def get_var_series(jae_data):
+def get_returns_over_horizon(ret, h):
+    ret_over_h = np.empty(len(ret) - h)
+
+    for i in range(len(ret) - h):
+        curr_ret = 1
+        for j in range(i, i + h + 1):
+            curr_ret *= 1 + ret[j]
+        ret_over_h[i] = curr_ret - 1
+
+    return ret_over_h
+
+
+def get_var_series(jae_data, horizon):
+    end = -horizon
     return (
-        np.array(transform_volatility_by_horizon(jae_data["RV"], 22)),
-        np.array(transform_volatility_by_horizon(jae_data["ORV"], 22)),
-        np.array(transform_volatility_by_horizon(jae_data["JV"], 22)),
-        np.array(transform_volatility_by_horizon(jae_data["TV"], 22)),
+        np.array(transform_volatility_by_horizon(jae_data["RV"], 22))[:end],
+        np.array(transform_volatility_by_horizon(jae_data["ORV"], 22))[:end],
+        np.array(transform_volatility_by_horizon(jae_data["JV"], 22))[:end],
+        np.array(transform_volatility_by_horizon(jae_data["TV"], 22))[:end],
     )
 
 
 def get_vrp(vix_series, rv_series):
-    return ((vix_series / 100) ** 2 - rv_series).to_numpy()
+    return (vix_series / 100) ** 2 - rv_series
 
 
 def get_jrp(jv_series, rv_series, tv_series):
     return jv_series - (rv_series - tv_series)
 
 
-def get_ols_summary(x, y):
-    return sm.OLS(y, sm.add_constant(x)).fit().summary()
+def get_ols_statistics(x, y):
+    model = sm.OLS(y, sm.add_constant(x)).fit()
+
+    return model.tvalues[1], model.rsquared
 
 
-def run_regressions(mkt, vrp_1, vrp_2, jrp):
-    print(get_ols_summary(jrp[:-1], mkt[1:]))
+def get_regression_statistics_for_regressor(regressor, mkt):
+    t_statistic, r_squared = get_ols_statistics(regressor, mkt)
+
+    return {"t_statistic": t_statistic, "r_squared": r_squared}
+
+
+def get_regression_statistics(mkt, vrp_1, vrp_2, jrp):
+    res_dict_for_horizon = dict()
+
+    res_dict_for_horizon["vrp_1"] = get_regression_statistics_for_regressor(vrp_1, mkt)
+    res_dict_for_horizon["vrp_2"] = get_regression_statistics_for_regressor(vrp_2, mkt)
+    res_dict_for_horizon["jrp"] = get_regression_statistics_for_regressor(jrp, mkt)
+
+    return res_dict_for_horizon
 
 
 def main():
-    vix_series = get_vix_close_data()
-    mkt_ret_series = get_mkt_excess_return_data()
     jae_data = get_jae_data()
-
     allowed_dates = set(jae_data["date"])
-    vix_series = trim_series(vix_series, allowed_dates)[21:]
-    mkt_ret_series = trim_series(mkt_ret_series, allowed_dates)
-    mkt_ret_series = transform_volatility_by_horizon(mkt_ret_series, 22)
+    vix_series = trim_series(get_vix_close_data(), allowed_dates)
+    mkt_ret_series = trim_series(get_mkt_excess_return_data(), allowed_dates)
 
-    rv_series, orv_series, jv_series, tv_series = get_var_series(jae_data)
+    res_dict = dict()
 
-    run_regressions(
-        mkt_ret_series,
-        get_vrp(vix_series, rv_series),
-        get_vrp(vix_series, orv_series),
-        get_jrp(jv_series, rv_series, tv_series),
-    )
+    for month in range(1, 13):
+        horizon = month * 21
+        curr_vix = vix_series[21:-horizon]
+        curr_mkt_ret = get_returns_over_horizon((mkt_ret_series / 100)[21:], horizon)
+
+        rv_series, orv_series, jv_series, tv_series = get_var_series(jae_data, horizon)
+
+        res_dict[f"horizon_{month}_month"] = get_regression_statistics(
+            curr_mkt_ret,
+            get_vrp(curr_vix, rv_series),
+            get_vrp(curr_vix, orv_series),
+            get_jrp(jv_series, rv_series, tv_series),
+        )
+
+    with open("return_predictability.json", "w") as f:
+        json.dump(res_dict, f)
 
 
 if __name__ == "__main__":
